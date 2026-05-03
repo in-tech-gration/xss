cf60526b6cfaac81fa2dfe1ca41c1cb3230436b6
Kostas Minaidis <minaidisk@gmail.com>
2026-05-04 02:38:20 +0300

    Update server script

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var session = require('express-session');
-var serverStatic = require('serve-static');
-var app = express();
+const express = require('express');
+const session = require('express-session');
+const app = express();
 
 // This serves the index.html file to the browser.
-app.use(serverStatic(__dirname + '/public'));
+app.use(express.static('public'));
+
+const PORT = 3000;
 
 // Sessions are used by web applications to remember data about specific users.
 // This is how when you login to a website, it remembers you for a while.
 app.use(session({
-	secret: 'some randomly generated secret',
-	resave: true,
-	saveUninitialized: true,
-	cookie: {
-		httpOnly: false,
-		secure: false
-	}
+  secret: 'some randomly generated secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: false,
+    secure: false
+  }
 }));
 
 // Start listening to requests on the local machine at port 3000.
-app.listen(3000, function() {
-	console.log('Server listening at localhost:3000');
+app.listen(PORT, function () {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
