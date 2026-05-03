1381f0f2ca46eaac2db5a953cab743061f1450d9
Kostas Minaidis <minaidisk@gmail.com>
2026-05-04 02:38:05 +0300

    Update evil-server script

--- evil-server.js
+++ evil-server.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const PORT = 3001;
 
-app.use(function(req, res, next) {
-	// Allows CORS requests:
-	res.header('Access-Control-Allow-Origin', '*');
-	next();
+app.use(function (req, res, next) {
+  // Allows CORS requests:
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-app.get('/cookie', function(req, res, next) {
-	console.log('GET /cookie');
-	console.log(req.query.data);
-	res.send('Thanks!');
+app.get('/cookie', function (req, res, next) {
+  console.log('GET /cookie');
+  console.log(req.query.data);
+  res.send('Thanks!');
 });
 
-app.get('/keys', function(req, res, next) {
-	console.log('GET /keys');
-	console.log(req.query.data);
-	res.send('I\'ll try to remember that..');
+app.get('/keys', function (req, res, next) {
+  console.log('GET /keys');
+  console.log(req.query.data);
+  res.send('I\'ll try to remember that..');
 });
 
-app.listen(3001, function() {
-	console.log('"Evil" server listening at localhost:3001');
+app.listen(PORT, function () {
+  console.log(`"Evil" server listening at http://localhost:${PORT}`);
 });
