6a7f942f31ab4df5065d899ae4b223a3c32671c9
Kostas Minaidis <minaidisk@gmail.com>
2026-05-04 13:54:48 +0300

    Add ESLint config sample

---
+++ eslint.config.sample.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import nounsanitized from "eslint-plugin-no-unsanitized";
+import html from "eslint-plugin-html"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}", "**/*.html"],
+    plugins: { js, nounsanitized, html },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      "nounsanitized/method": "error",
+      "nounsanitized/property": "error",
+    },
+  },
+]);
